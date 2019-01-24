d505b822c47fd9cdac74eb8f01f042def8fa6e34
Demetrius Palmer <demetrius.palmer@faulkner.edu>
2019-01-23 19:11:46 -0600

    Updated code with Separate .css file and .js file

---
+++ initMap.js
@@ -0,0 +1,27 @@
+function initMap() {
+        var infowindow
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 13,
+          center: {lat: 32.3792, lng: -86.3077}
+        });
+        var geocoder = new google.maps.Geocoder();
+
+        document.getElementById('submit').addEventListener('click', function() {
+          geocodeAddress(geocoder, map);
+        });
+      }
+
+      function geocodeAddress(geocoder, resultsMap) {
+        var address = document.getElementById('address').value;
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: resultsMap,
+              position: results[0].geometry.location
+            });
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
+      }
