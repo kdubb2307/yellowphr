80d287ec72aa608973c57a0e88b73340dacc3752
Demetrius Palmer <demetrius.palmer@faulkner.edu>
2019-02-02 22:16:06 -0600

    Updated Code with Working buttons. Now we just need to get the prices, open times, etc to appear in the infoWindow and work on the styling

--- initMap.js
+++ initMap.js
@@ -20,7 +20,7 @@
 	 //creates Google Place Service
 	 service = new google.maps.places.PlacesService(map);
 	 service.nearbySearch(request, callback);
- }
+ 
  
  function callback(results, status){
 	 if (status == google.maps.places.PlacesServiceStatus.OK) {
@@ -43,4 +43,100 @@
 			 infowindow.setContent(place.name);
 			 infowindow.open(map, this);
 		 });
+ }//end createMarker
+ 
+ }//end initMap
+ 
+ 
+ 
+ function parkMarkers(){
+	 
+	var montomgery= new google.maps.LatLng(32.3792, -86.3077);
+	 
+	 map= new google.maps.Map(document.getElementById('map'), {
+		 center: montomgery,
+		 zoom:13
+	 });
+	 
+	 //information for the nearbySearch
+	 var request={
+		 location:montomgery,
+		 radius: '4000',
+		 type: 'park'
+	 };
+	 
+	 //creates Google Place Service
+	 service = new google.maps.places.PlacesService(map);
+	 service.nearbySearch(request, callback);
+ 
+ 
+ function callback(results, status){
+	 if (status == google.maps.places.PlacesServiceStatus.OK) {
+		 for (var i= 0; i < results.length; i++) {
+			 var place = results[i];
+			 createMarker (results[i]);
+		 }
+	 }
  }
+ 
+ function createMarker(place) {
+	 var marker = new google.maps.Marker ({
+		 map:map,
+		 title: place.name,
+		 position: place.geometry.location});
+		 
+		 var infowindow = new google.maps.InfoWindow();
+		 
+		 google.maps.event.addListener(marker, 'click', function(){
+			 infowindow.setContent(place.name);
+			 infowindow.open(map, this);
+		 });
+ }//end createMarker 
+	 
+ }//end parkMarkers
+ 
+ function gymMarkers(){
+	 
+	var montomgery= new google.maps.LatLng(32.3792, -86.3077);
+	 
+	 map= new google.maps.Map(document.getElementById('map'), {
+		 center: montomgery,
+		 zoom:13
+	 });
+	 
+	 //information for the nearbySearch
+	 var request={
+		 location:montomgery,
+		 radius: '4000',
+		 type: 'gym'
+	 };
+	 
+	 //creates Google Place Service
+	 service = new google.maps.places.PlacesService(map);
+	 service.nearbySearch(request, callback);
+ 
+ 
+ function callback(results, status){
+	 if (status == google.maps.places.PlacesServiceStatus.OK) {
+		 for (var i= 0; i < results.length; i++) {
+			 var place = results[i];
+			 createMarker (results[i]);
+		 }
+	 }
+ }
+ 
+ function createMarker(place) {
+	 var marker = new google.maps.Marker ({
+		 map:map,
+		 title: place.name,
+		 position: place.geometry.location});
+		 
+		 var infowindow = new google.maps.InfoWindow();
+		 
+		 google.maps.event.addListener(marker, 'click', function(){
+			 infowindow.setContent(place.name);
+			 infowindow.open(map, this);
+		 });
+ }//end createMarker 
+	 
+ }//end gymMarkers
